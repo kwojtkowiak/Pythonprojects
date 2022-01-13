503583d5143fa95763250e804fe46e8c033e14ad
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-01-13 20:55:17 +0100

    Another version
    Started working on second version of metadata generator

--- metatada_creator/metadata_creator.py
+++ metatada_creator/metadata_creator.py
@@ -2,22 +2,27 @@ import json
 import os
 import os.path
 
-def generate_metadata(amount, config):
-    print("Generating {} .json files".format(amount))
+files = glob.glob(r"D:\GitHub\projectspython\Pythonprojects\metatada_creator\pics**")
+for file in files:
+    # Get the name of the file less any suffixes
+    name = os.path.basename(file).split('.')[0]
+    
+    # Serializing json 
+    json_object = json.dumps(dictionary, indent = 4)
+  
+    # Writing to sample.json
+    with open(name, "w") as outfile:
+    outfile.write(json_object)
+    # Use `name` from above to name your text file         
+    with open("D:\GitHub\projectspython\Pythonprojects\metatada_creator\pics**" + name + ".txt", 'w') as f:
+        f.write(str(extracted_texts))
+
 
 generate_unique_images(11, {
-
-    "name": "obiyankenobi",
-    "description": "Cool art for @obiyankenobi, courtesy of @PixtarsHathor",
-    "file": "ipfs://ipfs/QmYKrVVCdmf1tkThbmny1M8JHVGrLdWNnXhCnrUUrvzsZa",
-    "attributes": [
-        {
-            "type": "rarity",
-            "value": "super rare"
-        },
-        {
-            "type": "title",
-            "value": "CEO"
-        }
-    ]
+    
+    "baseURI": ".",
+    "name": "Faces #",
+    "description": "Random faces made by me.",
+    "file": "",
+    
 }
---
+++ metatada_creator/metadata_creator_alternative.py
@@ -0,0 +1,53 @@
+import json
+import os
+import os.path
+
+os.system('cls' if os.name=='nt' else 'clear')
+
+#input directory and creaty empty list for iterating through all files in the folder
+path = "D:\GitHub\projectspython\Pythonprojects\metatada_creator\pics"
+files = os.listdir(path)
+picfiles = []
+number_of_pics = len(picfiles)
+
+for file in files:
+    picfiles.append(file)
+
+def generate_unique_metadata(number_of_pics, config):
+    print("Generating {} unique metadata files".format(number_of_pics))
+    pad_amount = len(str(number_of_pics));
+
+    i = 1
+    for item in picfiles:
+        item["tokenId"] = i
+        i += 1
+
+    for i, token in enumerate(picfiles):
+        attributes = []
+        for key in token:
+            if key != "tokenId":
+                attributes.append({"trait_type": key, "value": token[key]})
+        token_metadata = {
+            "image": config["baseURI"] + "/images/" + str(token["tokenId"]) + '.png',
+            "tokenId": token["tokenId"],
+            "name":  config["name"] + str(token["tokenId"]).zfill(pad_amount),
+            "description": config["description"],
+            "attributes": attributes
+        }
+
+#Example metadata requirements
+{
+    "name": "FAC #",
+    "description": "Collection of PictoHeads I've made.",
+    "file": "<tu bedzie link>",
+    "attributes": [
+        {
+            "type": "rarity",
+            "value": "super rare"
+        },
+        {
+            "type": "title",
+            "value": "CEO"
+        }
+    ]
+}
